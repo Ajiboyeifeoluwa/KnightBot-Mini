4d08ab5f7e9b4a06e527e0283790f5edce31b288
ifeoluwa <ifeoluwashokoya6699@gmail.com>
2026-07-17 17:16:36 +0100

    updated few things

--- config.js
+++ config.js
@@ -4,19 +4,19 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['9876543210'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['2347057440801'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['Ripper Bot'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
-    botName: 'Knight Bot Mini',
-    prefix: ',',
+    botName: 'Ripper Bot Mini',
+    prefix: '.',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
     newsletterJid: '120363161513685998@newsletter', // Newsletter JID for menu forwarding
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
-    packname: 'Knight Bot',
+    packname: 'Ripper Bot',
     
     // Bot Behavior
     selfMode: false, // Private mode - only owner can use commands
